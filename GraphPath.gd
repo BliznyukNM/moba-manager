ee5fa42a922dcb67b4a4b045ac7027eb4a24a724
Nikita Bliznyuk <bliznyuknm@gmail.com>
2021-07-21 18:56:34 +0200

    added graph path visualization
    * first iteration of gameplay GraphPath.gd class
    * plugin for graph visualization
    * one line map iteration

---
+++ GraphPath.gd
@@ -0,0 +1,24 @@
+class_name GraphPath
+extends Node
+
+
+export var graph: Dictionary = {
+	NodePath("Radiant/Fountain"): {
+		NodePath("Radiant/T2"): 1
+	},
+	NodePath("Radiant/T2"): {
+		NodePath("Radiant/T1"): 1
+	},
+	NodePath("Radiant/T1"): {
+		NodePath("Mid"): 1
+	},
+	NodePath("Dire/Fountain"): {
+		NodePath("Dire/T2"): 1
+	},
+	NodePath("Dire/T2"): {
+		NodePath("Dire/T1"): 1
+	},
+	NodePath("Dire/T1"): {
+		NodePath("Mid"): 1
+	}
+}
---
+++ addons/path_graph/PathGraphPlugin.gd
@@ -0,0 +1,41 @@
+tool
+extends EditorPlugin
+
+
+var graphPath: GraphPath
+var font: DynamicFont
+
+const font_data = preload("res://addons/path_graph/SourceCodePro-Regular.ttf")
+
+
+func init_font() -> void:
+	font = DynamicFont.new()
+	font.font_data = font_data
+	font.size = 28
+
+
+func handles(object: Object) -> bool:
+	return object is GraphPath
+
+
+func edit(object: Object) -> void:
+	graphPath = object as GraphPath
+
+
+func forward_canvas_draw_over_viewport(overlay: Control) -> void:
+	if graphPath == null: return
+	if font == null: init_font()
+	
+	for node in graphPath.graph:
+		var pathData: Dictionary = graphPath.graph.get(node)
+		
+		for targetNode in pathData:
+			var a = graphPath.get_node_or_null(node) as Node2D
+			var b = graphPath.get_node_or_null(targetNode) as Node2D
+			
+			if a == null or b == null: continue
+			
+			var transform_viewport = graphPath.get_viewport().global_canvas_transform
+			
+			overlay.draw_line(transform_viewport * a.position, transform_viewport * b.position, Color.blue, 3.0)
+			overlay.draw_string(font, transform_viewport * (a.position.linear_interpolate(b.position, 0.5)), String(pathData.get(targetNode)))
