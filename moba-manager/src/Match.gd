727dc700aa061711e74605b989a5c0c0edd3386c
Nikita Bliznyuk <bliznyuknm@gmail.com>
2021-08-28 23:37:14 +0200

    working on test hero

--- moba-manager/src/Match.gd
+++ moba-manager/src/Match.gd
@@ -1,6 +1,26 @@
 extends Node
 
 
+enum HeroKey { Test }
+
+
+# TODO: move to some kind of hero database
+onready var heroes: Dictionary = {
+	HeroKey.Test: preload("res://src/heroes/TestHero.tscn")
+}
+
+
+onready var ui_root: MatchUI = $MatchUI
+onready var heroes_root: Node = $Heroes
+
+
 func _ready() -> void:
-	# generate heroes
-	pass
+	ui_root.add_radiant_hero(_create_hero(HeroKey.Test))
+	ui_root.add_dire_hero(_create_hero(HeroKey.Test))
+
+
+func _create_hero(hero_key: int) -> Hero:
+	var hero: Hero = heroes[hero_key].instance()
+	heroes_root.add_child(hero)
+	return hero
+
