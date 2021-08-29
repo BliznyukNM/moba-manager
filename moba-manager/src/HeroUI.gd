4c878a4c764f2d9ef9c2682614e4534bc398d69d
Nikita Bliznyuk <bliznyuknm@gmail.com>
2021-08-29 20:58:17 +0200

    implemented basic update system

--- moba-manager/src/HeroUI.gd
+++ moba-manager/src/HeroUI.gd
@@ -5,6 +5,7 @@ class_name HeroUI
 onready var name_label: Label = $Name
 onready var health_bar: TextureProgress = $Health
 onready var mana_bar: TextureProgress = $Mana
+onready var gold_label: Label = $Gold
 
 
 var _hero: Hero
@@ -16,9 +17,12 @@ func init(hero: Hero) -> void:
 	yield(self, "ready")
 	name_label.text = tr(hero.name_key)
 	health_bar.max_value = hero.max_health
+	mana_bar.max_value = hero.max_mana
 
 	update()
 
 
 func update() -> void:
 	health_bar.value = _hero.health
+	mana_bar.value = _hero.mana
+	gold_label.text = str(_hero.gold)
--- moba-manager/src/heroes/Hero.gd
+++ moba-manager/src/heroes/Hero.gd
@@ -9,13 +9,53 @@ export var base_attack: = 40
 export var name_key: String
 
 
-var max_health : int setget , get_max_health
+var max_health: int setget , get_max_health
 func get_max_health() -> int:
 	return base_health
 
 
-var health : int = self.max_health setget _set_health, get_health
+var health: int = self.max_health setget _set_health, get_health
 func _set_health(value: int) -> void:
-	health = int(clamp(value, 0, self._max_health))
+	health = int(clamp(value, 0, self.max_health))
 func get_health() -> int:
 	return health
+
+
+var max_mana: int setget , get_max_mana
+func get_max_mana() -> int:
+	return base_mana
+
+
+var mana: int = self.max_mana setget _set_mana, get_mana
+func _set_mana(value: int) -> void:
+	mana = int(clamp(value, 0, self.max_mana))
+func get_mana() -> int:
+	return mana
+
+
+var attack: int setget , get_attack
+func get_attack() -> int:
+	return base_attack
+
+
+var gold: int setget _set_gold, get_gold
+func _set_gold(value: int) -> void:
+	gold = value
+func get_gold() -> int:
+	return gold
+
+
+func update() -> void:
+	farm() # TODO: just testing
+
+
+func damage(value: int) -> void:
+	self.health -= value
+
+
+func farm() -> void:
+	self.gold += 1
+
+
+func attack_target(target: Hero) -> void:
+	target.damage(self.attack)	
