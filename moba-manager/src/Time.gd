0dcd0a3bfdac91ff4f882f11802a0995627939fd
Nikita Bliznyuk <bliznyuknm@gmail.com>
2021-09-11 14:16:17 +0200

    added time control to match UI

--- moba-manager/src/Time.gd
+++ moba-manager/src/Time.gd
@@ -1,10 +1,24 @@
 extends Node
 
 
-const updates_per_second: = 15
+const _updates_per_second: = 16
+const _max_multiply: = 64
 
 
 var _ticks_passed: int
+var _paused: bool
+
+var time_multiplicator: int = 1 setget , get_time_multiplicator # if < 0 -> slow down, if > 0 -> speed up
+func get_time_multiplicator() -> int:
+	return time_multiplicator
+
+
+func get_updates_per_second() -> int:
+	if _paused: return 0
+	if time_multiplicator < 0:
+		return _updates_per_second / -time_multiplicator
+	else:
+		return _updates_per_second * time_multiplicator
 
 
 func tick() -> void:
@@ -13,7 +27,25 @@ func tick() -> void:
 
 func get_passed_time() -> String:
 	return "%02d:%02d:%02d" % [
-		_ticks_passed / (updates_per_second * 60),
-		(_ticks_passed / updates_per_second) % 60,
-		_ticks_passed % updates_per_second
+		_ticks_passed / (_updates_per_second * 60),
+		(_ticks_passed / _updates_per_second) % 60,
+		_ticks_passed % _updates_per_second
 	]
+
+
+func toggle_play() -> void:
+	_paused = not _paused
+
+
+func slow_down() -> void:
+	if time_multiplicator > 1: time_multiplicator /= 2
+	elif time_multiplicator == 1: time_multiplicator = -2
+	else: time_multiplicator *= 2
+	time_multiplicator = int(max(time_multiplicator, -_updates_per_second))
+
+
+func speed_up() -> void:
+	if time_multiplicator < -2: time_multiplicator /= 2
+	elif time_multiplicator == -2: time_multiplicator = 1
+	else: time_multiplicator *= 2
+	time_multiplicator = int(min(time_multiplicator, _max_multiply))
