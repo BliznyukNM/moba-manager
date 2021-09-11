5370bf5b243560211739e5194c321c0323379dca
Nikita Bliznyuk <bliznyuknm@gmail.com>
2021-09-11 15:16:51 +0200

    small refactoring

--- moba-manager/src/Match.gd
+++ moba-manager/src/Match.gd
@@ -2,41 +2,41 @@ extends Node
 
 
 # TODO: move to some kind of hero database
-onready var heroes: = {
+onready var _heroes: = {
 	HeroKey.Test: preload("res://src/heroes/TestHero.tscn"),
 	HeroKey.SecondTest: preload("res://src/heroes/SecondTestHero.tscn")
 }
 
 
-onready var ui_root: = $MatchUI
-onready var heroes_root: = $Heroes
-onready var time: = $"/root/Time"
+onready var _ui_root: = $MatchUI
+onready var _heroes_root: = $Heroes
+onready var _time: = $"/root/Time"
 
 
 var _update_cooldown: float
 
 
 func _ready() -> void:
-	ui_root.add_hero(_create_hero(HeroKey.Test, Fraction.Radiant))
-	ui_root.add_hero(_create_hero(HeroKey.SecondTest, Fraction.Dire))
+	_ui_root.add_hero(_create_hero(HeroKey.Test, Fraction.Radiant))
+	_ui_root.add_hero(_create_hero(HeroKey.SecondTest, Fraction.Dire))
 
 
 func _create_hero(hero_key: int, fraction: int) -> Hero:
-	var hero: Hero = heroes[hero_key].instance()
+	var hero: Hero = _heroes[hero_key].instance()
 	hero.fraction = fraction
-	heroes_root.add_child(hero)
+	_heroes_root.add_child(hero)
 	return hero
 
 
 func _update() -> void:
-	for hero in heroes_root.get_children():
+	for hero in _heroes_root.get_children():
 		hero.update()
-	ui_root.update()
-	time.tick()
+	_ui_root.update()
+	_time.tick()
 
 
 func _physics_process(delta: float) -> void:
-	_update_cooldown += delta * time.get_updates_per_second()
+	_update_cooldown += delta * _time.get_updates_per_second()
 	var ticks_amount = int(_update_cooldown)
 
 	for _i in range(0, ticks_amount):
